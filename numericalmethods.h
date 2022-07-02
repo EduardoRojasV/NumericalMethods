a731568c47637c0f05da12fcca14489493982b43
EduardoRojasV <102326040+EduardoRojasV@users.noreply.github.com>
2022-07-01 21:29:44 -0500

    Add files via upload
    Se agrega una nueva clase (Método de integración Regla de Simpson 1/3)

--- numericalmethods.h
+++ numericalmethods.h
@@ -1,22 +1,44 @@
-#ifndef NUMERICALMETHODS_H
-#define NUMERICALMETHODS_H
-
 /** Descripción:
  *
  * Esta biblioteca está diseñada bajo el paradigma de programación Orientado a Objetos (POO),
  * para ayudar a los desarrolladores en C++ (Versión del estandar: 11) que requieran de métodos
  * numéricos en sus proyectos.
  *
- * @author Jose Eduardo Rojas Vera <joseeduardo2511@outlook.es> / GLINTEC
+ * @author Jose Eduardo Rojas Vera <joseeduardo2511@outlook.es>
  * @date Junio - 2022
  * @version 1.0.0
  *
  * This work is licensed under the terms of the GNU GPL, version 2.
 */
 
+#ifndef NUMERICALMETHODS_H
+#define NUMERICALMETHODS_H
+
 namespace NumericalMethods {
 
-// *** Integración
+template <typename T>
+struct Sample
+{
+    T sample;
+    T interval;
+
+    Sample(): sample(T()), interval(T())
+    {
+
+    }
+    Sample(const T s, const T i): sample(s), interval(i)
+    {
+
+    }
+
+    void clean()
+    {
+        sample   = static_cast<T>(0);
+        interval = static_cast<T>(0);
+    }
+};
+
+// *** Integración abierta de Newton-Cotes
 
 /** Descripción
  *
@@ -44,8 +66,7 @@ namespace NumericalMethods {
 template<typename T>
 class TrapezoidalRule
 {
-    T m_lastSample;
-    T m_lastInterval;
+    Sample<T> m_lastSample;
     T m_integral;
 
 public:
@@ -56,6 +77,55 @@ public:
     void integrate(const T newSample, const T newInterval);
 };
 
+template<typename T>
+class Simpson1_3
+{
+    Sample<T> m_firstPoint;
+    Sample<T> m_secondPoint;
+    Sample<T> m_thirdPoint;
+    T m_integral;
+
+public:
+    Simpson1_3() : m_firstPoint(Sample<T>()),
+    m_secondPoint(Sample<T>()),
+    m_thirdPoint(Sample<T>()),
+    m_integral(T())
+    {
+
+    }
+
+    T result() const { return m_integral; }
+    void resetValues()
+    {
+        m_firstPoint.clean();
+        m_secondPoint.clean();
+        m_thirdPoint.clean();
+        m_integral = static_cast<T>(0);
+    }
+    void setFirstPoint(const T newSample, const T newInterval)
+    {
+        m_firstPoint.sample   = newSample;
+        m_firstPoint.interval = newInterval;
+    }
+    void setSecondPoint(const T newSample, const T newInterval)
+    {
+        m_secondPoint.sample   = newSample;
+        m_secondPoint.interval = newInterval;
+    }
+    void setThirdPoint(const T newSample, const T newInterval)
+    {
+        m_thirdPoint.sample   = newSample;
+        m_thirdPoint.interval = newInterval;
+    }
+    void integrate()
+    {
+        T Ancho = ( m_thirdPoint.interval - m_firstPoint.interval );
+        T AlturaPromedio =
+                m_firstPoint.sample + (4 * m_secondPoint.sample) + m_thirdPoint.sample;
+        AlturaPromedio /= 6;
+        m_integral = AlturaPromedio;
+    }
+};
 // *** Derivación
 
 // *** Transformadas
