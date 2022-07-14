124b9ef9a53a532a1f7158d00c625d5f783d033d
EduardoRojasV <102326040+EduardoRojasV@users.noreply.github.com>
2022-07-13 21:46:06 -0500

    Add files via upload
    Se agregan un par algoritmos, de integración y de diferenciación numérica.

--- numericalmethods.cpp
+++ numericalmethods.cpp
@@ -2,32 +2,4 @@
 
 namespace NumericalMethods {
 
-// *** Metódos de Integración
-
-template<typename T>
-TrapezoidalRule<T>::TrapezoidalRule(): m_lastSample(Sample<T>()), m_integral(T())
-{
-
-}
-
-template<typename T>
-T TrapezoidalRule<T>::result() const { return m_integral; }
-
-template<typename T>
-void TrapezoidalRule<T>::resetValues()
-{
-    m_lastSample.clean();
-    m_integral = static_cast<T>(0);
-}
-
-template<typename T>
-void TrapezoidalRule<T>::integrate(const T newSample, const T newInterval)
-{
-    T area = (newInterval - m_lastSample.interval) * (m_lastSample.sample + newSample);
-    area /= static_cast<T>(2);
-    m_integral += area;
-    m_lastSample.sample   = newSample;
-    m_lastSample.interval = newInterval;
-}
-
 }
--- numericalmethods.h
+++ numericalmethods.h
@@ -14,123 +14,194 @@
 #ifndef NUMERICALMETHODS_H
 #define NUMERICALMETHODS_H
 
+#include <iostream>
+#include <array>
+
+#define DEBUG
+
 namespace NumericalMethods {
 
-template <typename T>
-struct Sample
-{
-    T sample;
+  template <typename T>
+  struct Sample
+  {
+    T value;
     T interval;
 
-    Sample(): sample(T()), interval(T())
+    Sample(): value(T()), interval(T()) { }
+    Sample(const T s, const T i): value(s), interval(i) { }
+    Sample(const Sample<T> &s): value(s.value), interval(s.interval) { }
+
+    void resetValue()
     {
-
+      value    = static_cast<T>(0);
+      interval = static_cast<T>(0);
     }
-    Sample(const T s, const T i): sample(s), interval(i)
-    {
+  };
 
-    }
+  // *** Integración de señales digitales con los métodos numéricos de Newton-Cotes
 
-    void clean()
-    {
-        sample   = static_cast<T>(0);
-        interval = static_cast<T>(0);
-    }
-};
-
-// *** Integración abierta de Newton-Cotes
-
-/** Descripción
+  /** Descripción de la clase TrapezoidalRule
  *
- * La clase TrapezoidalRule (Regla del Trapezio) utiliza el método
- * numérico del trapezio para integrar funcines. La implementación de
- * este algoritmo esta echa para integrar funciones construidas atravez
- * de la adquisicion de muestras obtenidas en intervalos (acumulado de
- * muestras).
+ * La clase TrapezoidalRule implenta el método numérico regla del trapezio,
+ * para integra funciones que son construidas tomado muestras en intervalos
+ * (regulares y no reglares) o con tabulación de datos en. El intervalo de
+ * intregación será definido por el consumidor de esta clase.
  *
  * Nota: Es importante recalcar que las disminución del erro en el calculo
- * esta relacinado con la frecuencia de muestreo, a mayor frecuencia de
- * muestreo se tiene una mejor aproximacion y por consiguiente una disminucion
+ * esta relacinado con la cantidad de muestras y el intervalo entre cada
+ * muestra, a mayor cantidad de muestras con intervalos pequeños entre cada
+ * una se tiene una mejor aproximacion y por consiguiente una disminucion
  * del erro.
- *
- * @param new sample and new interval
- *
- * sample: Mestra obtenida.
- *
- * interval: el intervalo en el que se tomó la muestra.
- *
- * @return el valor de la integral actualizado con la
- * con la ultima muestra e intervalo procesada.
 */
 
-template<typename T>
-class TrapezoidalRule
-{
+  template<typename T>
+  class TrapezoidalRule
+  {
     Sample<T> m_lastSample;
     T m_integral;
 
-public:
-    TrapezoidalRule();
-
-    T result() const;
-    void resetValues();
-    void integrate(const T newSample, const T newInterval);
-};
-
-template<typename T>
-class Simpson1_3
-{
-    Sample<T> m_firstPoint;
-    Sample<T> m_secondPoint;
-    Sample<T> m_thirdPoint;
-    T m_integral;
-
-public:
-    Simpson1_3() : m_firstPoint(Sample<T>()),
-    m_secondPoint(Sample<T>()),
-    m_thirdPoint(Sample<T>()),
-    m_integral(T())
-    {
-
-    }
+  public:
+    TrapezoidalRule(): m_lastSample(Sample<T>()), m_integral(T()) {  }
 
     T result() const { return m_integral; }
+
     void resetValues()
     {
-        m_firstPoint.clean();
-        m_secondPoint.clean();
-        m_thirdPoint.clean();
-        m_integral = static_cast<T>(0);
+      m_lastSample.resetValue();
+      m_integral = static_cast<T>(0);
     }
-    void setFirstPoint(const T newSample, const T newInterval)
+    void integrate(const T newSample, const T newInterval)
     {
-        m_firstPoint.sample   = newSample;
-        m_firstPoint.interval = newInterval;
+      T area = (newInterval - m_lastSample.interval) * (m_lastSample.value + newSample);
+      area /= static_cast<T>(2);
+      m_integral += area;
+      m_lastSample.value    = newSample;
+      m_lastSample.interval = newInterval;
     }
-    void setSecondPoint(const T newSample, const T newInterval)
+  };
+
+  template<typename T>
+  class Simpson_1_3
+  {
+    Sample<T> m_firstSample;
+    Sample<T> m_secondSample;
+    Sample<T> m_thirdSample;
+    T m_integral;
+
+  public:
+    Simpson_1_3() : m_firstSample(Sample<T>()),
+      m_secondSample(Sample<T>()),
+      m_thirdSample(Sample<T>()),
+      m_integral(T()) {  }
+
+    T result() const { return m_integral; }
+
+    void resetValues()
     {
-        m_secondPoint.sample   = newSample;
-        m_secondPoint.interval = newInterval;
+      m_firstSample.resetValue();
+      m_secondSample.resetValue();
+      m_thirdSample.resetValue();
+      m_integral = static_cast<T>(0);
     }
-    void setThirdPoint(const T newSample, const T newInterval)
+    void setFirstSample(const T newValue, const T newInterval)
     {
-        m_thirdPoint.sample   = newSample;
-        m_thirdPoint.interval = newInterval;
+      m_firstSample.value    = newValue;
+      m_firstSample.interval = newInterval;
     }
+    void setSecondSample(const T newValue, const T newInterval)
+    {
+      m_secondSample.value    = newValue;
+      m_secondSample.interval = newInterval;
+    }
+    void setThirdSample(const T newValue, const T newInterval)
+    {
+      m_thirdSample.value    = newValue;
+      m_thirdSample.interval = newInterval;
+    }
+
     void integrate()
     {
-        T Ancho = ( m_thirdPoint.interval - m_firstPoint.interval );
-        T AlturaPromedio =
-                m_firstPoint.sample + (4 * m_secondPoint.sample) + m_thirdPoint.sample;
-        AlturaPromedio /= 6;
-        m_integral = AlturaPromedio;
+      T width { m_thirdSample.interval - m_firstSample.interval };
+      T secondSample = (m_secondSample.value * static_cast<T>(4));
+      T AverageHeight = (m_firstSample.value + secondSample + m_thirdSample.value) / static_cast<T>(6);
+
+      m_integral = width * AverageHeight;
     }
-};
-// *** Derivación
+  };
 
-// *** Transformadas
+  // *** Diferenciación numérica
 
-// *** Estadística
+  // Primera diferencia hacia adelante
+
+  template<typename T>
+  class FirstDifference
+  {
+    Sample<T> m_lastSample;
+    T m_derivative;
+
+  public:
+    FirstDifference(): m_lastSample(Sample<T>()), m_derivative(T()) {}
+
+    T result() const { return m_derivative; }
+
+    void resetValues()
+    {
+      m_lastSample.resetValue();
+      m_derivative = static_cast<T>(0);
+    }
+    void derive(const T newValue, const T newInterval)
+    {
+      T denominator = m_lastSample.interval - newInterval;
+      T numerator   = m_lastSample.value - newValue;
+
+      m_derivative = denominator != static_cast<T>(0) ? (numerator/denominator) : static_cast<T>(0);
+
+      m_lastSample.interval = newInterval;
+      m_lastSample.value    = newValue;
+    }
+  };
+
+  // *** Transformadas
+
+  template <typename T, uint8_t S>
+  class CircularSamples
+  {
+    std::array<Sample<T>, S> m_samples;
+    Sample<T> *m_sampleCurrent;
+    uint8_t m_size = S;
+
+    void nextSample()
+    {
+      ++m_sampleCurrent;
+      if(m_sampleCurrent == m_samples.end())
+        m_sampleCurrent = m_samples.begin();
+    }
+
+  public:
+
+    CircularSamples(): m_size(S)
+    {
+      m_sampleCurrent = m_samples.begin();
+    }
+
+    uint8_t size() const { return m_size; }
+    const Sample<T> &sampleCurrent() const { return *m_sampleCurrent; }
+
+    void setNextSample(const Sample<T> &newvalue)
+    {
+      *m_sampleCurrent = newvalue;
+      nextSample();
+    }
+
+    void setNextSample(const T newvalue, const T interval)
+    {
+      m_sampleCurrent->value    = newvalue;
+      m_sampleCurrent->interval = interval;
+      nextSample();
+    }
+  };
+
+  // *** Estadística
 
 }
 #endif // NUMERICALMETHODS_H
